c633ff35d56e5f6beb234c954aae01e86e8ee491
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-28 00:08:51 +0300

    Update mongo.py

--- AasthaMusicBot/core/mongo.py
+++ AasthaMusicBot/core/mongo.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. Yukki
-
 from motor.motor_asyncio import AsyncIOMotorClient as _mongo_client_
 from pymongo import MongoClient
 
