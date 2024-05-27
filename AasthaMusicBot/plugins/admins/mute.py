abc69b8a217ce33bebc04a7f08232a8fef20fafd
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-28 00:19:21 +0300

    Update mute.py

--- AasthaMusicBot/plugins/admins/mute.py
+++ AasthaMusicBot/plugins/admins/mute.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. Yukki
-
 from pyrogram import filters
 from pyrogram.types import Message
 
@@ -19,7 +9,7 @@ from AasthaMusicBot.utils.database import is_muted, mute_on
 from AasthaMusicBot.utils.decorators import AdminRightsCheck
 
 # Commands
-MUTE_COMMAND = get_command("MUTE_COMMAND")
+MUTE_COMMAND = get_command("sessiz")
 
 
 @app.on_message(filters.command(MUTE_COMMAND) & filters.group & ~BANNED_USERS)
