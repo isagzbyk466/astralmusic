4eb0e29b22ee615c1763bcdfeec806daa298eb97
isagzbyk466 <167032463+isagzbyk466@users.noreply.github.com>
2024-05-28 00:08:02 +0300

    Update dir.py

--- AasthaMusicBot/core/dir.py
+++ AasthaMusicBot/core/dir.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
-
-# Kanged By © @Dr_Asad_Ali
-# Rocks © @Shayri_Music_Lovers
-# Owner Asad Ali
-# Harshit Sharma
-# All rights reserved. Yukki
-
 import os
 import sys
 from os import listdir, mkdir
@@ -18,7 +8,7 @@ from ..logging import LOGGER
 def dirr():
     if "assets" not in listdir():
         LOGGER(__name__).warning(
-            f"Assets Folder not Found. Please clone repository again."
+            f"Varlık Klasörü Bulunamadı. Lütfen depoyu tekrar klonlayın."
         )
         sys.exit()
     for file in os.listdir():
